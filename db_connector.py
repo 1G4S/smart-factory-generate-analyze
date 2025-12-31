6d3f5bc68e2ea9ce07e8c568ef2c008e01f5df0c
Igor Sarnowski <igor.sarnowski@student.put.poznan.pl>
2025-12-31 23:30:47 +0100

    feat(db_connector): Add function connect_db that allows to connect to local database

---
+++ db_connector.py
@@ -0,0 +1,34 @@
+import pyodbc
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+DRIVER=os.getenv('DRIVER')
+SERVER=os.getenv('SERVER')
+DATABASE=os.getenv('DATABASE')
+
+def connect_db(driver, server, database):
+    """
+    Function that allows to connect to local database.
+
+    :param driver: Name of the driver on local db
+    :param server: Name of server on local db
+    :param database: Name of local db
+    :return: Connection to database
+    """
+    connection_string = (
+        f'DRIVER={driver};'
+        f'SERVER={server};'
+        f'DATABASE={database};'
+        f'Trusted_Connection=yes;'
+        f'TrustServerCertificate=yes;'
+    )
+    try:
+        conn = pyodbc.connect(connection_string, timeout=15)
+        print('Connection to the database established!')
+
+    except pyodbc.Error as e:
+        print('There was an error during connection!')
+        raise e
+
+    return conn
