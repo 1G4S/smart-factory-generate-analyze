948a290715944c03b785a2eb2a550f146337bea4
Igor Sarnowski <igor.sarnowski@student.put.poznan.pl>
2026-01-01 18:24:26 +0100

    feat: Add functions that allows to update events data, and insert production data

--- database/db_insertion.py
+++ database/db_insertion.py
@@ -14,32 +14,14 @@ def telemetry_insertion(conn, machine_id, date_key, timestamp, temperature, vibr
     :param power_usage:
     :return:
     """
-
-    sql_query = ("INSERT INTO fct_Telemetry ("
-                "MachineID, "
-                "DateKey, "
-                "Timestamp, "
-                "Temperature, "
-                "VibrationLevel, "
-                "RPM, "
-                "PowerUsage"
-            ") "
-             "VALUES ("
-                "?,"
-                "?,"
-                "?,"
-                "?,"
-                "?,"
-                "?,"
-                "?,"
-             ")")
+    sql_query = ("EXEC LoadTelemetry @MachineID=?, @DateKey=?, @Timestamp=?, @Temperature=?, @VibrationLevel=?, @RPM=?, "
+                 "@PowerUsage=?")
 
     cursor = conn.cursor()
     cursor.execute(sql_query, machine_id, date_key, timestamp, temperature, vibration_level, rpm, power_usage)
     print(f'[LOG]: Insertion to fct_Telemetry: MachineID :{machine_id} | DateKey: {date_key} '
           f'| Timestamp: {timestamp} | Temperature: {temperature} | VibrationLevel: {vibration_level} '
           f'| RPM: {rpm} | PowerUsage: {power_usage}')
-    conn.commit()
 
 def machine_events_insertion(conn, machine_id, failure_id, start_time, end_time, state_code):
     """
@@ -66,12 +48,37 @@ def machine_events_insertion(conn, machine_id, failure_id, start_time, end_time,
                     "?,"
                     "?,"
                     "?,"
-                    "?,"
+                    "?"
                  ")"
                  )
 
     cursor = conn.cursor()
     cursor.execute(sql_query, machine_id, failure_id, start_time, end_time, state_code)
-    print(f'[LOG]: Insertion to fct_Telemetry: MachineID :{machine_id} | FailureID: {failure_id} '
+    print(f'[LOG]: Insertion to fct_MachineEvents: MachineID :{machine_id} | FailureID: {failure_id} '
           f'| StartTime: {start_time} | EndTime: {end_time} | StateCode: {state_code} ')
-    conn.commit()
+
+
+def machine_events_update(conn, end_time, machine_id):
+    """
+    Function that inserts given values to fct_MachineEvents table in SmartFactory database.
+    It also prints info log about values being inserted to this table. All params are generated in python simulator.
+    :param conn:
+    :param machine_id:
+    :param end_time:
+    :return:
+    """
+
+    sql_query = "UPDATE fct_MachineEvents SET EndTime = ? WHERE EndTime IS NULL AND MachineID = ?"
+
+    cursor = conn.cursor()
+    cursor.execute(sql_query, end_time, machine_id)
+    print(f'[LOG]: Update in MachineEvents: MachineID :{machine_id} '
+          f' | EndTime: {end_time}')
+
+def production_output_insertion(conn, machine_id, product_id, is_good, is_scrap):
+    sql_query = """
+    INSERT INTO fct_ProductionOutput (MachineID, ProductID, IsGood, IsScrap)
+    VALUES (?, ?, ?, ?)
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql_query, machine_id, product_id, is_good, is_scrap)
